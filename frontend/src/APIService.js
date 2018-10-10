ed1d0b7ffeb3d038c18adde4520aea604abc1c37
gracri <chrisgraff@protonmail.com>
2018-10-10 20:04:38 +0200

    init vue proj, api service

---
+++ frontend/src/APIService.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+const API_URL = 'http://localhost:8000';
+
+export class APIService{
+
+  constructor(){
+  }
+
+  getContacts() {
+      const url = `${API_URL}/api/contacts/`;
+      return axios.get(url).then(response => response.data);
+  }
+
+  getContact(pk) {
+      const url = `${API_URL}/api/contacts/${pk}`;
+      return axios.get(url).then(response => response.data);
+  }
+
+  createContact(contact){
+    const url = `${API_URL}/api/contacts/`;
+    return axios.post(url,contact);
+  }
+
+  updateContact(contact){
+    const url = `${API_URL}/api/contacts/${contact.pk}`;
+    return axios.put(url,contact);
+  }
+
+  deleteContact(contact){
+    const url = `${API_URL}/api/contacts/${contact.pk}`;
+    return axios.delete(url);
+  }
+
+}
